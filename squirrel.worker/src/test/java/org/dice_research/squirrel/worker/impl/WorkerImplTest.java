a2a4568bf90ae42e02a8e1a5928e84876d41c708
ajay <kp.tendulkar@gmail.com>
2019-01-24 11:41:17 +0100

    Successfully implemented the testcase for WorkerImpl

--- squirrel.worker/src/test/java/org/dice_research/squirrel/worker/impl/WorkerImplTest.java
+++ squirrel.worker/src/test/java/org/dice_research/squirrel/worker/impl/WorkerImplTest.java
@@ -3,6 +3,8 @@ package org.dice_research.squirrel.worker.impl;
 import com.mongodb.MongoClient;
 import crawlercommons.fetcher.http.SimpleHttpFetcher;
 import crawlercommons.fetcher.http.UserAgent;
+import org.apache.jena.rdf.model.Model;
+import org.dice_research.squirrel.MongoDBBasedTest;
 import org.dice_research.squirrel.analyzer.Analyzer;
 import org.dice_research.squirrel.analyzer.manager.SimpleOrderedAnalyzerManager;
 import org.dice_research.squirrel.collect.SqlBasedUriCollector;
@@ -26,9 +28,7 @@ import org.dice_research.squirrel.data.uri.serialize.Serializer;
 import com.rethinkdb.RethinkDB;
 import com.rethinkdb.net.Connection;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -36,9 +36,9 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import org.dice_research.squirrel.sink.impl.file.FileBasedSink;
+import java.util.Map;
 
-import java.io.File;
+import org.dice_research.squirrel.sink.impl.file.FileBasedSink;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -51,47 +51,14 @@ public class WorkerImplTest {
     static MongoDBQueue queue;
     static MongoDBKnowUriFilter filter;
     static List<CrawleableUri> uris = new ArrayList<CrawleableUri>();
-    protected File tempDirectory = null;
+    protected File outputDirectory = null;
     static CrawleableUriFactory4Tests cuf = new CrawleableUriFactory4Tests();
-    private static  RethinkDB r;
-    private static Connection connection;
-    protected static  MongoClient client;
-    File  outputDir = null ;
     WorkerImpl worker;
-
-    @BeforeClass
-    public static void setUpMDB() throws Exception {
-
-        String mongoDockerStopCommand = "docker stop squirrel-test-mongodb";
-        Process p = Runtime.getRuntime().exec(mongoDockerStopCommand);
-        p.waitFor();
-        String mongoDockerRmCommand = "docker rm squirrel-test-mongodb";
-        p = Runtime.getRuntime().exec(mongoDockerRmCommand);
-        p.waitFor();
-        String mongoDockerExecCmd = "docker run --name squirrel-test-mongodb "
-            + "-p 58027:27017 -p 58886:8080 -d mongo:4.0.0";
-         p = Runtime.getRuntime().exec(mongoDockerExecCmd);
-        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String s = null;
-        while ((s = stdInput.readLine()) != null) {
-            System.out.println(s);
-        }
-        // read any errors from the attempted command
-        BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-        System.out.println("Here is the standard error of the command (if any):\n");
-        while ((s = stdError.readLine()) != null) {
-            System.out.println(s);
-        }
-
-        client = new MongoClient(DB_HOST_NAME,DB_PORT);
-
-    }
-
-
-
+    boolean check = false;
 
     @Test
     public void test() throws Exception{
+        MongoDBBasedTest.setUpMDB();
         filter = new MongoDBKnowUriFilter("localhost", 58027);
         queue = new MongoDBQueue("localhost", 58027);
         filter.open();
@@ -99,39 +66,56 @@ public class WorkerImplTest {
         frontier = new FrontierImpl(new NormalizerImpl(), filter, queue,true);
         uris.add(cuf.create(new URI("http://dbpedia.org/resource/New_York"), InetAddress.getByName("127.0.0.1"),
             UriType.DEREFERENCEABLE));
-        uris.add(cuf.create(new URI("http://dbpedia.org/resource/Moscow"), InetAddress.getByName("127.0.0.1"),
-           UriType.DEREFERENCEABLE));
         queue.addCrawleableUri(uris.get(0));
-//       queue.addCrawleableUri(uris.get(2));
-        tempDirectory = File.createTempFile("FileBasedSinkTest", ".tmp");
-        Assert.assertTrue(tempDirectory.delete());
-        Assert.assertTrue(tempDirectory.mkdir());
-        tempDirectory.deleteOnExit();
-       // Sink sink = new FileBasedSink(outputDir, true) ;
-        Sink sink = new FileBasedSink(tempDirectory, FileBasedSink.DEFAULT_OUTPUT_LANG, true);
+        outputDirectory = File.createTempFile("FileBasedSinkTest", ".tmp");
+        Assert.assertTrue(outputDirectory.delete());
+        Assert.assertTrue(outputDirectory.mkdir());
+        outputDirectory.deleteOnExit();
+        Sink sink = new FileBasedSink(outputDirectory, FileBasedSink.DEFAULT_OUTPUT_LANG, true);
         Serializer serializer = new GzipJavaUriSerializer();
         String dbdir = null;
         dbdir = TempFileHelper.getTempDir("dbTest", "").getAbsolutePath() + File.separator + "test";
         UriCollector collector = SqlBasedUriCollector.create(serializer, dbdir) ;
-       Analyzer analyzer = new SimpleOrderedAnalyzerManager(collector);
+        Analyzer analyzer = new SimpleOrderedAnalyzerManager(collector);
         worker = new WorkerImpl(frontier,sink,analyzer,new RobotsManagerImpl(new SimpleHttpFetcher(new UserAgent("Test", "", ""))),
             serializer, collector, 100, null, true);
-        worker.run();
-        Iterator<byte[]> it = worker.collector.getUris(uris.get(1));
-        Assert.assertEquals(8681,it);
-        Assert.assertEquals(1,worker.fetcher.fetch(uris.get(1)));
-        System.out.println("done");
+        Thread t = new Thread(worker);
+        t.start();
+        do {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Assert.fail(e.getLocalizedMessage());
+            }
+        } while ((frontier.getNumberOfPendingUris() > 0));
+        worker.setTerminateFlag(true);
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            Assert.fail(e.getMessage());
+        }
+        //checks if the outputdirectory is not null that means the crawling is done and the .ttl's have been stored in the FileBasedSink       Assert.assertNotNull(outputDirectory);
+        File[] listOfFiles = outputDirectory.listFiles();
+        for (File f : listOfFiles){
+            f.getName().endsWith(".ttl"); //default language of FileBasedSink
+            check = true;
+        }
+        Assert.assertTrue(true);
     }
 
 
 
     @AfterClass
     public static void tearDownMDB() throws Exception {
-        String mongoDockerStopCommand = "docker stop squirrel-test-mongodb";
-        Process p = Runtime.getRuntime().exec(mongoDockerStopCommand);
-        p.waitFor();
-        String mongoDockerRmCommand = "docker rm squirrel-test-mongodb";
-        p = Runtime.getRuntime().exec(mongoDockerRmCommand);
-        p.waitFor();
+        filter.purge();
+        queue.purge();
+        String dockerStopCommand = "docker stop squirrel-test-workerimpl";
+        Process q = Runtime.getRuntime().exec(dockerStopCommand);
+        q.waitFor();
+        String dockerRmCommand = "docker rm squirrel-test-workerimpl";
+        q = Runtime.getRuntime().exec(dockerRmCommand);
+        q.waitFor();
+
     }
 }
